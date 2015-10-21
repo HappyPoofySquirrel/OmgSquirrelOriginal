edf7ac0045ef3595969080e8e3efc66b48a9fb05
happypoofysquirrel <guyver432@gmail.com>
2015-10-21 12:54:55 -0400

    start

--- app/src/main/java/com/example/sleazypmartini/omgsquirrel2/MainActivity.java
+++ app/src/main/java/com/example/sleazypmartini/omgsquirrel2/MainActivity.java
@@ -21,14 +21,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       // FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-       // fab.setOnClickListener(new View.OnClickListener() {
-       //     @Override     //message button i dont want
-       //     public void onClick(View view) {
-       //         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-       //                 .setAction("Action", null).show();
-       //     }
-       // });
+
         Button button = (Button) findViewById(R.id.factsButton);    //added to link button +method below
         button.setOnClickListener(new View.OnClickListener() {
             @Override
