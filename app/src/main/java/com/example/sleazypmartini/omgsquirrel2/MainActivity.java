eb41629cd7cebc1418685469f7b0a819e1b250b0
happypoofysquirrel <guyver432@gmail.com>
2015-10-30 09:36:58 -0400

    icon

--- app/src/main/java/com/example/sleazypmartini/omgsquirrel2/MainActivity.java
+++ app/src/main/java/com/example/sleazypmartini/omgsquirrel2/MainActivity.java
@@ -50,20 +50,20 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+  //  @Override             this adds a settings button
+  //  public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+      //  int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+     //   if (id == R.id.action_settings) {
+     //       return true;
+     //   }
 
-        return super.onOptionsItemSelected(item);
-    }
+      //  return super.onOptionsItemSelected(item);
+  //  }
     private void goToFactsActivity(){
         Intent intent =new Intent(this, FactsActivity.class);
         startActivity(intent);
