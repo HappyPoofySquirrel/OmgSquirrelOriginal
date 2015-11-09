c158f933f4ff3d8d32e6aab94202486a9f7d1a59
happypoofysquirrel <guyver432@gmail.com>
2015-11-09 09:02:05 -0500

    start

--- app/src/main/java/com/example/sleazypmartini/omgsquirrel2/FactsActivity.java
+++ app/src/main/java/com/example/sleazypmartini/omgsquirrel2/FactsActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
 import android.view.View;
 
 public class FactsActivity extends AppCompatActivity {
@@ -15,10 +16,18 @@ public class FactsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_facts);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        WebView myWebView = (WebView) findViewById(R.id.myWebView);
+        myWebView.loadUrl("file:///android_asset/Webview.html");
+
+
+        myWebView.setBackgroundColor(0x00000000);
+
         getSupportActionBar().setDisplayShowHomeEnabled(true);  //add these 2 lines to display icon
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
 
     }
+
 
 }
