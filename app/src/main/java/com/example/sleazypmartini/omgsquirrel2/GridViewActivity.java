fc4db19ab2da1148a9fa481934f1181f45bece91
happypoofysquirrel <guyver432@gmail.com>
2016-01-14 11:21:07 -0500

    start

--- app/src/main/java/com/example/sleazypmartini/omgsquirrel2/GridViewActivity.java
+++ app/src/main/java/com/example/sleazypmartini/omgsquirrel2/GridViewActivity.java
@@ -32,7 +32,8 @@ public class GridViewActivity extends AppCompatActivity {
     private ProgressBar mProgressBar;
     private GridViewAdapter mGridAdapter;
     private ArrayList<GridItem> mGridData;
-    private String FEED_URL = "http://javatechig.com/?json=get_recent_posts&count=45";
+    private String FEED_URL = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=206dcb0ade4879da809339697bb014e3&photoset_id=72157661135553275&user_id=137813339%40N03&format=json&nojsoncallback=1&auth_token=72157663290515282-01572e12a642b83f&api_sig=3ff57d063f8af8e088599c7fd2d5a553";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +83,7 @@ public class GridViewActivity extends AppCompatActivity {
     //Downloading data asynchronously
     public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
 
+
         @Override
         protected Integer doInBackground(String... params) {
             Integer result = 0;
@@ -92,70 +94,111 @@ public class GridViewActivity extends AppCompatActivity {
                 int statusCode = httpResponse.getStatusLine().getStatusCode();
 
                 // 200 represents HTTP OK
-                if (statusCode == 200) {
+                if (statusCode == 200) { //was ==
                     String response = streamToString(httpResponse.getEntity().getContent());
                     parseResult(response);
                     result = 1; // Successful
                 } else {
-                    result = 0; //"Failed
-                }
+                    result = 0;
+                     }
             } catch (Exception e) {
                 Log.d(TAG, e.getLocalizedMessage());
             }
             return result;
         }
 
-        @Override
-        protected void onPostExecute(Integer result) {
+        @Override  //this happens last
+        protected void onPostExecute(Integer result) { //Integer Result
             // Download complete. Let us update UI
             if (result == 1) {
                 mGridAdapter.setGridData(mGridData);
             } else {
-                Toast.makeText(GridViewActivity.this, "Failed to fetch data!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(GridViewActivity.this, "Failed to load data", Toast.LENGTH_SHORT).show();
             }
-            mProgressBar.setVisibility(View.GONE);
+           mProgressBar.setVisibility(View.GONE);
         }
     }
 
     String streamToString(InputStream stream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
-        String line;
-        String result = "";
-        while ((line = bufferedReader.readLine()) != null) {
-            result += line;
-        }
 
-        // Close stream
-        if (null != stream) {
-            stream.close();
+        BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder stringBuilder = new StringBuilder();
+        String result="";
+        String stringReadLine = null;
+        while ((stringReadLine = bufferedreader.readLine()) != null)                     {
+            stringBuilder.append(stringReadLine + "\n");
         }
+        result = stringBuilder.toString();
+
         return result;
     }
 
     /**
      * Parsing the feed results and get the list
      * @param result
-     */
-    private void parseResult(String result) {
+     **/
+
+
+    void parseResult(String result) {
+        GridItem item =null;
         try {
             JSONObject response = new JSONObject(result);
-            JSONArray posts = response.optJSONArray("posts");
+            JSONObject photoSet = response.getJSONObject("photoset");
+            JSONArray photos = photoSet.getJSONArray("photo");
+
+
+            for (int i = 0; i < photos.length(); i++) {
+
+                JSONObject temp_photo = photos.optJSONObject(i);
+
+                String id = temp_photo.getString("id");
+                String secret = temp_photo.getString("secret");
+                String server = temp_photo.getString("server");
+                String farm = temp_photo.getString("farm");
+                String title = temp_photo.getString("title");
+
+
+
+                        item.setImage("http://farm" + farm + ".static.flickr.com/" + server + "/" + id + "_" + secret + ".jpg");
+
+                mGridData.add(item);
+
+            }
+
+
+        } catch (JSONException e) {
+   Log.d("didn't work","Try again");
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+            e.printStackTrace();
+        }
+
+
+    }
+
+    /**
+     * Parsing the feed results and get the list
+     * @param result
+     */
+  /**  private void parseResult(String result) {
+        try {
+            JSONObject response = new JSONObject(result);
+            JSONArray photos = response.optJSONArray("photo");
             GridItem item;
-            for (int i = 0; i < posts.length(); i++) {
-                JSONObject post = posts.optJSONObject(i);
-                String title = post.optString("title");
+            for (int i = 0; i < photos.length(); i++) {
+                JSONObject photo = photos.getJSONObject(i);
                 item = new GridItem();
 
-                JSONArray attachments = post.getJSONArray("attachments");
-                if (null != attachments && attachments.length() > 0) {
-                    JSONObject attachment = attachments.getJSONObject(0);
-                    if (attachment != null)
-                        item.setImage(attachment.getString("url"));
-                }
+                String farm_id = photo.getString(farm_id);
+                String server_id = photo.getString(server_id);
+                String id = photo.getString(id);
+                String secret = photo.getString(secret);
+
+                item.setImage("http://farm" + item.farm_id + ".static.flickr.com/" + item.server_id + "/" + item.id + "_" + item.secret + "_m.jpg");
+
                 mGridData.add(item);
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
-    }
+    } **/
 }
